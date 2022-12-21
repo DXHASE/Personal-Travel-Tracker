8abf9ceed53b12e9d3c4f51a48693f157a3096a7
mmarsch88 <matthew.marsch@gmail.com>
2022-12-21 15:25:45 -0600

    Update Sports.js

--- client/src/pages/Sports.js
+++ client/src/pages/Sports.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import exampleImg from '../assets/exampleimg.png'
 
 const Sports = () => {
     return (
@@ -8,33 +9,48 @@ const Sports = () => {
             </div>
             <div className="flex-column justify-space-between p-3">
                 <section className="card">
-                    <h1 className="card-header">Check out our sports Clubs!</h1>
+                    <h1 className="card-header">San Antonio Spurs</h1>
                     <div className="card-body">
-
+                    <meat className="container flex-row justify-space-between">
+                            <img src={exampleImg} className="display-inline" style={{ width: "40%"}} alt="example"/>
+                            <p className="flex-row">The Spurs are San Antonio's premier basketball team. The 5 time NBA Champions are a persistent presence in the league's playoffs. The team plays it's home games at the AT&T Center on the city's Southeast side.</p>
+                        </meat>
                     </div>
                 </section>
                 <section className="card">
-                    <h1 className="card-header">Check out our sports Clubs!</h1>
+                    <h1 className="card-header">San Antonio Missions</h1>
                     <div className="card-body">
-
+                    <meat className="container flex-row justify-space-between">
+                            <img src={exampleImg} className="display-inline" style={{ width: "40%"}} alt="example"/>
+                            <p className="flex-row">The Missions are San Antonio's Minor League Baseball team and the Double-A affiliate of the San Diego Padres. The team is named for the Spanish missions around which the city was founded. The Missions play their home games at Nelson W. Wolff Municipal Stadium on the city's Southwest side.</p>
+                        </meat>
                     </div>
                 </section>
                 <section className="card">
-                    <h1 className="card-header">Check out our sports Clubs!</h1>
+                    <h1 className="card-header">San Antonio Football Club</h1>
                     <div className="card-body">
-
+                    <meat className="container flex-row justify-space-between">
+                            <img src={exampleImg} className="display-inline" style={{ width: "40%"}} alt="example"/>
+                            <p className="flex-row">San Antonio FC is San Antonio's professional soccer club. The team competes in the USL Championship, the second-highest level of the United States soccer league system. Although reletively new to the city, SAFC has a loyal fanbase who regularly pack Toyota Field Stadium, where the team plays on the city's Northeast side.</p>
+                        </meat>
                     </div>
                 </section>
                 <section className="card">
-                    <h1 className="card-header">Check out our sports Clubs!</h1>
+                    <h1 className="card-header">UTSA</h1>
                     <div className="card-body">
-
+                    <meat className="container flex-row justify-space-between">
+                            <img src={exampleImg} className="display-inline" style={{ width: "40%"}} alt="example"/>
+                            <p className="flex-row">The University of Texas at San Antonio is the city's largest college, and its football team reflects that. UTSA regularly regularly packs the Alamodome, where the team plays in the heart of the city.</p>
+                        </meat>
                     </div>
                 </section>
                 <section className="card">
-                    <h1 className="card-header">Check out our sports Clubs!</h1>
+                    <h1 className="card-header">San Antonio Brahmas</h1>
                     <div className="card-body">
-
+                    <meat className="container flex-row justify-space-between">
+                            <img src={exampleImg} className="display-inline" style={{ width: "40%"}} alt="example"/>
+                            <p className="flex-row">The Brahmas are the newest addition to the city's growing sports scene. While the XFL team's season won't begin until 2023, fans will undoubtedly be ecstatic for for their arrival at the Alomodome, the city's leading multi-purpose facility.</p>
+                        </meat>
                     </div>
                 </section>
             </div>
