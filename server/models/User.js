165a1521b47f25db57b83f0dc0186efc805fbb16
DXHASE <danielchas886@gmail.com>
2022-12-21 10:18:07 -0600

    Installed bcrypt

--- server/models/User.js
+++ server/models/User.js
@@ -1,23 +1,4 @@
 const { Schema, model } = require('mongoose');
-
-const userSchema = new Schema({
-    username: {
-        type: String
-    },
-    email: {
-        type: String
-    },
-    createdAt: {
-        
-    }
-});
-
-const User = model('User',userSchema);
-
-module.exports = User;
-
-
-const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema(
